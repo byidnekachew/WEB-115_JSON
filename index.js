dc15f731729551a44601f908365f56b87d1e1402
Biruk Yidnekachew <byidnekachew@my.waketech.edu>
2025-04-24 13:20:17 -0400

    Added code

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 console.log("Hello World")
+console.log(" ")
 
+const person = 
+`{
+    "firstName": "Biruk",
+    "lastName": "Yidnekachew",
+    "course": "WEB115",
+    "section": "2816",
+    "role": "students"
+}`
+
+console.log("Person object:")
+const personParse = JSON.parse(person)
+console.log(personParse)
+
+console.log(" ")
+
+console.log("JSON Format:")
+console.log(person)
